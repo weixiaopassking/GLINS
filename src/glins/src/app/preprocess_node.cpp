275dcaf34bd48d627c1ca5888076e3294c30b245
gang <niu_wengang@163.com>
2023-02-12 02:41:26 +0800

    更新

--- src/glins/src/app/preprocess_node.cpp
+++ src/glins/src/app/preprocess_node.cpp
@@ -1,35 +1,41 @@
 /*
- * @Description: preprocess sensor data
+ * @Description: 传感器数据预处理
  * @Function:
  * @Author: niu_wengang@163.com
  * @Version : v1.0
  * @Date: 2022-11-30
+ * @Moify: 2023-02-12
  */
 
-
-// relevent
 #include "../../include/preprocess/PreprocessFlow.hpp"
-// ros
+// ros库
 #include <ros/ros.h>
 #include <ros/package.h>
+// c++库
 #include <signal.h>
-// thirdpart lib
-// tools
+// 第三方库
+
+// 调试工具
 #include "../../include/tools/ColorTerminal.hpp"
 
 using namespace glins;
 
+/**
+ * @brief  中断检测
+ * @note
+ * @todo
+ **/
 void mySigintHandler(int sig)
 {
     ColorTerminal::NodeInfo("[preprocess_node] shutdown");
     ros::shutdown();
-     exit (0);
+    exit(0);
 }
 
 int main(int argc, char **argv)
 {
 
-    /*config ros */
+    /*ros配置 */
     ros::init(argc, argv, "preprocess_node", ros::init_options::NoSigintHandler);
     ros::start();
     ros::NodeHandle nh;
@@ -37,12 +43,12 @@ int main(int argc, char **argv)
     signal(SIGINT, mySigintHandler);
     ColorTerminal::NodeInfo("[preprocess_node] start");
 
-    /*config glog*/
+    /*日志配置*/
 
-    /*config flow*/
+    /*流程控制层创建*/
     std::shared_ptr<PreprocessFlow> preprocess_flow_ptr = std::make_shared<PreprocessFlow>(nh);
 
-    /*execute circlue */
+    /*执行循环 */
     while (ros::ok())
     {
         ros::spinOnce();
