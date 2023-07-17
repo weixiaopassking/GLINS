fc1ae0291efdce6f3f7896eb10d713e97b589800
niu_wengang <niu_wengang@163.com>
2023-07-17 20:40:27 +0800

    update

--- auto_save.py
+++ auto_save.py
@@ -4,11 +4,11 @@ import sys
 if __name__ == '__main__':
     param_num=len(sys.argv)
     print(param_num)
-    write_log=''
+    write_log="update"
     if len==2 :
-        write_log = sys.argv[1]
+        write_log = '"'+sys.argv[1]+'"'
     os.system('git add .')
-    os.system('git commit -m "update"')
+    os.system('git commit -m %s' % (write_log))
     print(write_log)
     os.system('git push')
     if len==3 :
