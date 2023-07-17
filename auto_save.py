a752ebab028945e79033d11d90e82ed0dd066362
niu_wengang <niu_wengang@163.com>
2023-07-17 20:46:36 +0800

    增加暴力匹配

--- auto_save.py
+++ auto_save.py
@@ -5,14 +5,14 @@ if __name__ == '__main__':
     param_num=len(sys.argv)
     print(param_num)
     write_log="update"
-    if len==2 :
+    if param_num==2 :
         write_log = '"'+sys.argv[1]+'"'
         print(write_log)
     os.system('git add .')
     os.system('git commit -m %s' % (write_log))
     print(write_log)
     os.system('git push')
-    if len==3 :
+    if param_num==3 :
          time_delay=sys.argv[2]
          os.system('shutdown -h %d' % (time_delay))
  
