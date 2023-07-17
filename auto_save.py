d5ca4e48eef2d23663ce473d3fafd3a65acbd265
niu_wengang <niu_wengang@163.com>
2023-07-17 21:03:32 +0800

    增加暴力匹配

--- auto_save.py
+++ auto_save.py
@@ -2,19 +2,28 @@
 import os
 import sys
 
+class bcolor(object):
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+
+
 if __name__ == '__main__':
+
     param_num=len(sys.argv)
     print(param_num)
     write_log="update"
     if param_num>=2 :
         write_log = sys.argv[1]
-        print(write_log)
     os.system('git add .')
     os.system('git commit -m %s' % (write_log))
-    print(write_log)
+    print( bcolor.OKBLUE+ 'push 修改为:'+write_log + bcolor.FAIL)
     os.system('git push')
     if param_num>=3 :
-         time_delay=sys.argv[2]
-         print('程序已push '+time_delay+' min后关机')
-         os.system('shutdown -h '+ time_delay)
- 
+        time_delay=sys.argv[2]
+        print( bcolor.OKBLUE + '程序已push '+time_delay+' min后关机' + bcolor.FAIL)
+        os.system('shutdown -h '+ time_delay)
