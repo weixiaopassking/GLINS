39048d2e07a36d04a9d102d0afe94ef0fd660e3a
niu_wengang <niu_wengang@163.com>
2023-07-17 20:34:13 +0800

    update

--- auto_save.py
+++ auto_save.py
@@ -7,15 +7,13 @@ import sys
 if __name__ == '__main__':
     write_log='update'
     param_num=len(sys.argv)
-    if len==1 :
+    if len==2 :
         write_log = sys.argv[1]
-
-
     os.system('git add .')
     os.system('git commit -m'+write_log)
     print(write_log)
     os.system('git push')
-    if len==2 :
+    if len==3 :
          time_delay=sys.argv[2]
          os.system('shutdown -h %d' % (time_delay))
  
