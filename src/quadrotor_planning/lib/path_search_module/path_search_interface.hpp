3b3ae464f352883c75f5578ffcf8ccba62e9c5b8
niu_wengang <niu_wengang@163.com>
2023-07-22 11:42:35 +0800

    update

---
+++ src/quadrotor_planning/lib/path_search_module/path_search_interface.hpp
@@ -0,0 +1,12 @@
+#ifndef _PATH_SEARCH_INTERFACE
+#define _PATH_SEARCH_INTERFACE
+
+class PathSearchInterface
+{
+    PathSearchInterface();
+    virtual void SetMap();
+    // virtual SetTast(Node start_point,Node end_point);
+    // virtual Node* GetPath();
+};
+
+#endif //_PATH_SEARCH_INTERFACE
